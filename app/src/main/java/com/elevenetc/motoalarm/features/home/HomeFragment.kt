5dc26827257244b201ac57e9143faa88c66d4dfa
eugene.levenetc <eugene.levenetc@themobilecompany.com>
2019-06-02 23:17:31 +0200

    Add home layouts

--- app/src/main/java/com/elevenetc/motoalarm/features/home/HomeFragment.kt
+++ app/src/main/java/com/elevenetc/motoalarm/features/home/HomeFragment.kt
@@ -1,10 +1,17 @@
 package com.elevenetc.motoalarm.features.home
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import com.elevenetc.motoalarm.R
 import com.elevenetc.motoalarm.core.ui.BaseFragment
 
 class HomeFragment : BaseFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
---
+++ app/src/main/java/com/elevenetc/motoalarm/features/signin/SignInActivity.kt
@@ -0,0 +1,20 @@
+package com.elevenetc.motoalarm.features.signin
+
+import android.os.Bundle
+import com.elevenetc.motoalarm.R
+import com.elevenetc.motoalarm.core.ui.BaseActivity
+
+class SignInActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        appComponent.mainActivity().activity = this
+
+        if (savedInstanceState == null) {
+            appComponent.navigation().root().start()
+        }
+    }
+
+
+}
